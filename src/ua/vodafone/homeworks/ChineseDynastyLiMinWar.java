9d6462d3498063bbc2eaff85c118f8e713eaca0f
InnaLogvin <107686161+InnaLogvin@users.noreply.github.com>
2023-10-03 17:56:02 +0300

    Update test

--- src/ua/vodafone/homeworks/ChineseDynastyLiMinWar.java
+++ src/ua/vodafone/homeworks/ChineseDynastyLiMinWar.java
@@ -12,12 +12,14 @@ public class ChineseDynastyLiMinWar {
         int minArmySize = (int) (liArmySize * 1.5);
         System.out.println("Dynasty Li attack level is: " + calculateAttackLevel(liWarrior, liArcher, liCavalry, liArmySize));
         System.out.println("Dynasty Min attack level is: " + calculateAttackLevel(minWarrior, minArcher, minCavalry, minArmySize));
+        System.out.println("test");
     }
 
     public static int calculateAttackLevel(int warrior, int archer, int cavalry, int armySize) {
         return armySize * (warrior + archer + cavalry);
 
     }
+
 }
 
 

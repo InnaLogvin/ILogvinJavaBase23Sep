45eaceef4d2d518c0f50c82a35951237f8c41f67
InnaLogvin <107686161+InnaLogvin@users.noreply.github.com>
2023-10-17 13:30:52 +0300

    Added array persons

--- src/ua/vodafone/homeworks/classes/Person.java
+++ src/ua/vodafone/homeworks/classes/Person.java
@@ -19,15 +19,14 @@ public class Person {
     }
 
     public static void main(String[] args) {
-        Person person1 = new Person("Will", "Smith", "New York", "2936729462846");
-        System.out.println(person1.personInfo());
-
-        Person person2 = new Person("Jackie", "Chan", "Shanghai", "12312412412");
-        System.out.println(person2.personInfo());
-
-        Person person3 = new Person("Sherlock", "Holmes", "London", "37742123513");
-        System.out.println(person3.personInfo());
-
+        Person[] persons = {
+                new Person("Will", "Smith", "New York", "2936729462846"),
+                new Person("Jackie", "Chan", "Shanghai", "12312412412"),
+                new Person("Sherlock", "Holmes", "London", "37742123513")
+        };
+        System.out.println(persons[0].personInfo());
+        System.out.println(persons[1].personInfo());
+        System.out.println(persons[2].personInfo());
     }
 }
 

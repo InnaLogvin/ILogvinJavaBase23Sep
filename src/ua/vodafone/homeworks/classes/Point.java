df798d79fdf0aac3846212a8ec7343f0241d4428
InnaLogvin <107686161+InnaLogvin@users.noreply.github.com>
2023-10-24 15:23:17 +0300

    Changed distanceTo and distanceBetween. Added copy and clone

--- src/ua/vodafone/homeworks/classes/Point.java
+++ src/ua/vodafone/homeworks/classes/Point.java
@@ -1,6 +1,6 @@
 package ua.vodafone.homeworks.classes;
 
-public class Point implements PointCalculable {
+public class Point implements PointCalculable, Cloneable {
     private int x;
     private int y;
 
@@ -9,42 +9,48 @@ public class Point implements PointCalculable {
         this.y = y;
     }
 
-    public int getX() {
-        return x;
+    public Point(Point point) {
+        x = point.x;
+        y = point.y;
     }
 
-    public int getY() {
-        return y;
+    public int getX() {
+        return x;
     }
 
     public void setX(int x) {
         this.x = x;
     }
 
+    public int getY() {
+        return y;
+    }
+
     public void setY(int y) {
         this.y = y;
     }
 
-    public int distanceTo(Point another) { //відстань до іншої точки; //Відстань = √((x2 - x1)² + (y2 - y1)²)
-        int deltaX = this.x - another.x; //можно сделать дабл, но нужно ли?
+    public int distanceTo(Point another) { //відстань до іншої точки; відстань = √((x2 - x1)² + (y2 - y1)²)
+        return distanceBetween(this, another);
+    }
+
+    public int distanceBetween(Point one, Point another) {
+        int deltaX = this.x - another.x;
         int deltaY = this.y - another.y;
         return (int) Math.sqrt(deltaX * deltaX + deltaY * deltaY);
     }
 
-    public int distanceBetween(Point one, Point another) { //відстань між двома точками;
-        int deltaX = one.x - another.x;
-        int deltaY = one.y - another.y;
-        return (int) Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
     }
-
-    ;
 
     @Override
     public String toString() {
         return "point: (" + x + ", " + y + ")";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
         Point a = new Point(4, 5); //точка на площині
         System.out.println("a " + a.toString()); //отримати поточні координати
         Point b = new Point(1, 2);
@@ -54,14 +60,22 @@ public class Point implements PointCalculable {
         int distanceTo = a.distanceTo(b);
         System.out.println("Distance from a to b: " + distanceTo);
 
-        a.setX(1);
-        a.setY(3);
+        a.setX(9);
+        a.setY(6);
         System.out.println("a " + a.toString()); //отримати поточні координати після зміни координати
+        Point fakePoint = new Point(7, 3);
 
-        int distanceBetween = a.distanceBetween(a, b);
+        int distanceBetween = fakePoint.distanceBetween(a, b);
         System.out.println("Distance between a and b: " + distanceBetween);
 
         boolean equalPoint = a.equals(b);
         System.out.println("Result of comparison points a and b: " + equalPoint);
+
+        Point copiedPoint = new Point(fakePoint);
+        System.out.println("Copied fakePoint is " + copiedPoint);
+
+        Point clonedPoint = (Point) fakePoint.clone();
+        System.out.println("Cloned fakePoint is " + clonedPoint);
+
     }
 }
--- src/ua/vodafone/homeworks/classes/PointCalculable.java
+++ src/ua/vodafone/homeworks/classes/PointCalculable.java
@@ -2,6 +2,7 @@ package ua.vodafone.homeworks.classes;
 
 public interface PointCalculable {
     public int distanceTo(Point another);
-    public int distanceBetween(Point one, Point another);
+
+    public  int distanceBetween(Point one, Point another);
 
 }

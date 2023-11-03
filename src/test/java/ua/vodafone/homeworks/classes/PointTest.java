43597b14c69ef1b9def3f08d6d50744837fa7f78
InnaLogvin <107686161+InnaLogvin@users.noreply.github.com>
2023-11-03 13:14:05 +0200

    Upd env

--- src/test/java/ua/vodafone/homeworks/classes/PointTest.java
+++ src/test/java/ua/vodafone/homeworks/classes/PointTest.java
@@ -1,5 +1,6 @@
 package ua.vodafone.homeworks.classes;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,27 +9,41 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PointTest {
     private Point a;
     private Point b;
+    private double expectedDistance;
 
     @BeforeEach
     void setUp() {
-        Point a = new Point(4, 5);
-        Point b = new Point(1, 2);
+        a = new Point(4, 5);
+        b = new Point(1, 2);
+        expectedDistance = 4.242640687119285; // ОР расстояние между a і b
     }
-
-    //given - set up
-
 
     @Test
     void distanceTo() {
+        //given - set up
         //when
-        double expectedDistance = 4.242640687119285; // ОР расстояние между a і b
-
         double actualDistance = a.distanceTo(b);
         //then
-        assertEquals(expectedDistance, actualDistance, 0.0001); // проверка на равность с дельтой 0.0001
+        Assertions.assertEquals(expectedDistance, actualDistance); // проверка на равность
     }
 
     @Test
     void distanceBetween() {
+        //given - set up
+        //when
+        double actualDistance = Point.distanceBetween(a, b); // проверка на равность
+        //then
+        Assertions.assertEquals(expectedDistance, actualDistance);
+    }
+
+    @Test
+    void testDistanceBetweenNull() {
+        Point a = new Point(4, 5);
+        Point b = null; // проверка налл
+
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            Point.distanceBetween(a, b);
+        });
+
     }
 }
